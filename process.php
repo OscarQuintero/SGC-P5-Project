c46925b123d341b39a7046cfc442eda1c57b02e3
Oscar Quintero <oscar24quintero@hotmail.com>
2019-01-14 00:14:17 -0600

    Clonación de proceso 5 y 10 para generar la plantilla
    Sólo hay html

--- process.php
+++ process.php
@@ -1,1 +1,163 @@
-process.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SGC P5 - #</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/lateral-bar.css">
+</head>
+<body>
+	<header>
+		
+	</header>
+	<div class="main-container">
+	<aside>
+		<h2>Menú lateral</h2>
+		<ul>
+			<li>Mejora continua</li>
+			<li>No conformidad</li>
+			<li>Acciones correctivas</li>
+		</ul>
+	</aside>
+	<main class="process">
+		<h1>Proceso X: ############</h1>
+
+		<article>
+			<h2>Mejora continua</h2>
+			
+			<div>
+				<h3>Mapeos</h3>
+				<ul>
+					
+				</ul>
+			</div>
+			<div>
+				<h3>Documentos</h3>
+				<ul>
+					<li>Proyecto de mejora</li>
+					<li>Control de estatus de proyectos de mejora</li>
+				</ul>	
+			</div>
+			<div>
+				<h3>Formatos</h3>
+				<ul>
+					
+				</ul>
+
+			</div>
+			<div>
+				<h3>Especificaciones</h3>
+				<ul>
+					
+				</ul>
+			</div>
+			<div>
+				<h3>Instructivos</h3>
+				<ul>
+					
+				</ul>
+			</div>
+			<div>
+				<h3>Guías</h3>
+				<ul>
+					
+				</ul>
+			</div>		
+		</article>
+		<article>
+			<h2>No conformidad</h2>
+			
+			<div>
+				<h3>Mapeos</h3>
+				<ul>
+					
+				</ul>
+			</div>
+			<div>
+				<h3>Documentos</h3>
+				<ul>
+					
+				</ul>
+			</div>
+			<div>
+				<h3>Formatos</h3>
+				<ul>
+					<li>Producto No Conforme</li>
+					<li>Control de estatus de PNC o Queja</li>
+					<li>Queja, PNC, Sugerencia</li>
+					<li>Respuesta al usuario de queja o sugerencia</li>
+					<li>Matriz de criterio de producto no conforme</li>
+				</ul>
+
+			</div>
+			<div>
+				<h3>Especificaciones</h3>
+				<ul>
+					
+				</ul>
+			</div>
+			<div>
+				<h3>Instructivos</h3>
+				<ul>
+					
+				</ul>
+			</div>
+			<div>
+				<h3>Guías</h3>
+				<ul>
+					
+				</ul>
+			</div>		
+		</article>
+		<article>
+			<h2>Acciones correctivas</h2>
+			
+			<div>
+				<h3>Mapeos</h3>
+				<ul>
+					
+				</ul>
+			</div>
+			<div>
+				<h3>Documentos</h3>
+				<ul>
+					<li>Accion correctiva</li>
+					<li>Acción correctiva</li>
+					<li>Análisis de causa raíz</li>
+					<li>Control de estatus de no conformidades</li>
+				</ul>
+			</div>
+			<div>
+				<h3>Formatos</h3>
+				<ul>
+					
+				</ul>
+
+			</div>
+			<div>
+				<h3>Especificaciones</h3>
+				<ul>
+					
+				</ul>
+			</div>
+			<div>
+				<h3>Instructivos</h3>
+				<ul>
+					
+				</ul>
+			</div>
+			<div>
+				<h3>Guías</h3>
+				<ul>
+					
+				</ul>
+			</div>		
+		</article>				
+	</main>
+
+	</div>
+	<footer>
+		
+	</footer>
+</body>
+</html>
