f9f6d8e80923118e35ada76ead67c3fa50ac58ad
Oscar Quintero <oscar24quintero@hotmail.com>
2019-01-14 18:51:48 -0600

    Procesamiento de arreglo de subprocesoso en process.php
    Se porcesó correctamente el arreglo de los subprocesos y ya es posible rellenar e menu lateral con ellos así como la lista principal de los mismos

--- process.php
+++ process.php
@@ -26,8 +26,22 @@
 	$firstRow = $result->fetch_assoc();
 	$NombreProcesoMatrizActual = $firstRow['Nombre'];
 
-	// cerrar conexión
-	$connection->close();
+	//Obtener subprocesos
+	$sqlQuery = "SELECT Nombre FROM Subproceso WHERE IDProcesoMatriz = $IDProcesoMatrizActual;";
+	$result = $connection->query($sqlQuery);
+	if ($result->num_rows <= 0) {
+		die("No se pudo completar la solicitud a la BD... :'(");
+	}
+///***********************************************
+
+	$CantidadDeSubprocesos = 0;
+	$ListaDeSubprocesos = array();
+	while ($row = $result->fetch_assoc()) {
+		$ListaDeSubprocesos[$CantidadDeSubprocesos] = $row['Nombre'];
+		$CantidadDeSubprocesos++;
+	}
+
+	
 ?>
 <!DOCTYPE html>
 <html>
@@ -45,145 +59,61 @@
 	<aside>
 		<h2><?php echo $NombreProcesoMatrizActual;?></h2>
 		<ul>
-			<li>Mejora continua</li>
-			<li>No conformidad</li>
-			<li>Acciones correctivas</li>
+		<?php
+			for($k =0; $k < $CantidadDeSubprocesos; $k++) 
+			{
+			?>
+			<li><?php echo $ListaDeSubprocesos[$k];?></li>
+			<?php					
+			}
+		?>
+			
 		</ul>
 	</aside>
 	<main class="process">
 		<h1>Proceso <?php echo $IDProcesoMatrizActual;?>: <?php echo $NombreProcesoMatrizActual;?></h1>
 
-		<article>
-			<h2>Mejora continua</h2>
-			
-			<div>
-				<h3>Mapeos</h3>
-				<ul>
-					
-				</ul>
-			</div>
-			<div>
-				<h3>Documentos</h3>
-				<ul>
-					<li>Proyecto de mejora</li>
-					<li>Control de estatus de proyectos de mejora</li>
-				</ul>	
-			</div>
-			<div>
-				<h3>Formatos</h3>
-				<ul>
-					
-				</ul>
+		<?php
+			for($k =0; $k < $CantidadDeSubprocesos; $k++) 
+			{
+			?>
+			<article>
+				<h2><?php echo $ListaDeSubprocesos[$k];?></h2>
+				<div>
+					<h3>Mapeos</h3>
+					<ul>						
+					</ul>
+				</div>
+				<div>
+					<h3>Documentos</h3>
+					<ul>
+						
+					</ul>	
+				</div>
+				<div>
+					<h3>Formatos</h3>
+					<ul>
+						
+					</ul>
 
-			</div>
-			<div>
-				<h3>Especificaciones</h3>
-				<ul>
-					
-				</ul>
-			</div>
-			<div>
-				<h3>Instructivos</h3>
-				<ul>
-					
-				</ul>
-			</div>
-			<div>
-				<h3>Guías</h3>
-				<ul>
-					
-				</ul>
-			</div>		
-		</article>
-		<article>
-			<h2>No conformidad</h2>
-			
-			<div>
-				<h3>Mapeos</h3>
-				<ul>
-					
-				</ul>
-			</div>
-			<div>
-				<h3>Documentos</h3>
-				<ul>
-					
-				</ul>
-			</div>
-			<div>
-				<h3>Formatos</h3>
-				<ul>
-					<li>Producto No Conforme</li>
-					<li>Control de estatus de PNC o Queja</li>
-					<li>Queja, PNC, Sugerencia</li>
-					<li>Respuesta al usuario de queja o sugerencia</li>
-					<li>Matriz de criterio de producto no conforme</li>
-				</ul>
+				</div>
+				<div>
+					<h3>Instructivos</h3>
+					<ul>
+						
+					</ul>
+				</div>
+				<div>
+					<h3>Guías</h3>
+					<ul>
+						
+					</ul>
+				</div>		
 
-			</div>
-			<div>
-				<h3>Especificaciones</h3>
-				<ul>
-					
-				</ul>
-			</div>
-			<div>
-				<h3>Instructivos</h3>
-				<ul>
-					
-				</ul>
-			</div>
-			<div>
-				<h3>Guías</h3>
-				<ul>
-					
-				</ul>
-			</div>		
-		</article>
-		<article>
-			<h2>Acciones correctivas</h2>
-			
-			<div>
-				<h3>Mapeos</h3>
-				<ul>
-					
-				</ul>
-			</div>
-			<div>
-				<h3>Documentos</h3>
-				<ul>
-					<li>Accion correctiva</li>
-					<li>Acción correctiva</li>
-					<li>Análisis de causa raíz</li>
-					<li>Control de estatus de no conformidades</li>
-				</ul>
-			</div>
-			<div>
-				<h3>Formatos</h3>
-				<ul>
-					
-				</ul>
-
-			</div>
-			<div>
-				<h3>Especificaciones</h3>
-				<ul>
-					
-				</ul>
-			</div>
-			<div>
-				<h3>Instructivos</h3>
-				<ul>
-					
-				</ul>
-			</div>
-			<div>
-				<h3>Guías</h3>
-				<ul>
-					
-				</ul>
-			</div>		
-		</article>				
+			</article>
+			<?php					
+			}
+		?>				
 	</main>
 
 	</div>
@@ -192,3 +122,6 @@
 	</footer>
 </body>
 </html>
+<?php
+	$connection->close();
+?>
